093c33fcf40687de25c37184952fa4dab5564895
mark <m.arklee@outlook.com>
2019-06-27 21:42:43 -0700

    Finish vue basic

--- Vue Basics/app.js
+++ Vue Basics/app.js
@@ -4,7 +4,26 @@ new Vue({
     title: "Becoming a Vue ninja",
     url: "http://www.youtube.com",
     classes: ['one', 'two'],
-    wage: 10
+    wage: 10,
+    name: "Mark",
+    name2: "Peter",
+    m: "class",
+    toggle: true,
+    state: 1,
+    one: "one",
+    two: "two",
+    three: "three",
+    array: ["one", "two", "three"],
+    object: {
+      a: "a",
+      b: "b",
+      c: "c"
+    },
+
+    coord: {
+      x: 0,
+      y: 0,
+    }
   },
   methods: {
     greet() {
@@ -20,6 +39,24 @@ new Vue({
 
     logEvent(e) {
       console.log(e);
+    },
+
+    logCoord(e) {
+      this.coord.x = e.offsetX;
+      this.coord.y = e.offsetY;
+    },
+
+    updateName({target}) {
+      this.name = target.value;
+    },
+
+    toggling() {
+      this.toggle = !this.toggle;
+    },
+
+    changeNumber() {
+      this.state++;
+      if (this.state === 4) this.state = 1;
     }
   }
 });
